d6d345e8f0d22283be3d8dd8173eb3720f8fe48f
Nina-mvH <nv5842@princeton.edu>
2026-05-12 16:47:57 -0400

    test: add preliminary tests

---
+++ tests/unit/text/chat_completion/test_params.py
@@ -0,0 +1,13 @@
+from tigerflow_ml.text.chat_completion._base import _ChatCompletionBase
+
+
+def test_translate_defaults():
+    p = _ChatCompletionBase.Params()
+    assert p.allow_fetch is False
+    assert p.system_message is None
+    assert p.max_tokens == 512
+    assert p.max_model_len is None
+    assert p.max_image_size == 1024
+    assert p.revision == "main"
+    assert p.device == "auto"
+    assert p.cache_dir == ""
