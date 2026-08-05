a5a578831079c0c34883e3d8c027d22b195a0213
Nina <133538278+Nina-mvH@users.noreply.github.com>
2026-08-05 10:50:33 -0400

    test(embed): add integration test for embed task (#179)
    * adds an integration test for the embed task
    * implements new helper 
    assert_or_update_array_snapshot() for comparing np arrays
    *updates the use_encode_query to default to False

---
+++ tests/integration/test_embed.py
@@ -0,0 +1,53 @@
+"""Integration tests for Embed task."""
+
+import numpy as np
+import pytest
+
+from tigerflow_ml.text.embed._base import _EmbedBase
+
+from .conftest import assert_or_update_array_snapshot
+
+
+@pytest.fixture(scope="session")
+def embed_dir(test_dir):
+    return test_dir / "embed"
+
+
+@pytest.fixture(scope="module")
+def default_context(make_context):
+    import gc
+
+    import torch
+
+    ctx = make_context(_EmbedBase.Params, "embed")
+    _EmbedBase.setup(ctx)
+    yield ctx
+    del ctx.embedder
+    gc.collect()
+    torch.cuda.empty_cache()
+
+
+def test_setup(default_context):
+    assert default_context.embedder is not None
+
+
+def test_run(
+    default_context,
+    embed_dir,
+    get_input_files,
+    make_output_path,
+    snapshot_dir,
+    update_snapshots,
+):
+    for input_file in get_input_files(embed_dir):
+        output_file = make_output_path(input_file, ".npy")
+        _EmbedBase.run(default_context, input_file, output_file)
+
+        embedding = np.load(output_file)
+        assert_or_update_array_snapshot(
+            embedding,
+            f"embed/{input_file.stem}.npy",
+            snapshot_dir,
+            update_snapshots,
+            threshold=0.99,
+        )
