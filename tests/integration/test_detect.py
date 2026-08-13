0fdac57fe4123c43081eaa57b7f8cfe281963df1
Nina-mvH <nv5842@princeton.edu>
2026-08-13 12:31:58 -0400

    test(detect): update integration test to cover more supported params

--- tests/integration/test_detect.py
+++ tests/integration/test_detect.py
@@ -13,7 +13,14 @@ from .conftest import assert_or_update_snapshot
 def default_context(make_context):
     import torch
 
-    ctx = make_context(_DetectBase.Params, "detect")
+    ctx = make_context(
+        _DetectBase.Params,
+        "detect",
+        seed=84,
+        threshold=0.35,
+        batch_size=3,
+        sample_fps=1.2,
+    )
     _DetectBase.setup(ctx)
     yield ctx
     del ctx.pipeline
