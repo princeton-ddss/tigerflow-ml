a54a466154dc7bbfa1247a012a87a80827fc2b62
Nina-mvH <nv5842@princeton.edu>
2026-08-13 15:01:14 -0400

    test(translate): remove tests already covered by unit tests

--- tests/integration/test_translate.py
+++ tests/integration/test_translate.py
@@ -1,7 +1,5 @@
 """Integration tests for Translate task."""
 
-import copy
-
 import pytest
 
 from tigerflow_ml.text.translate._base import _TranslateBase
@@ -15,7 +13,7 @@ def default_context(make_context):
 
     import torch
 
-    ctx = make_context(_TranslateBase.Params, "translate", max_model_len=5000, seed=84)
+    ctx = make_context(_TranslateBase.Params, "translate")
     _TranslateBase.setup(ctx)
     yield ctx
     del ctx.translator
@@ -43,57 +41,6 @@ def test_run(
         assert_or_update_snapshot(
             text,
             f"translate/{input_file.stem}.txt",
-            snapshot_dir,
-            update_snapshots,
-            threshold=0.9,
-        )
-
-
-def test_run_with_custom_prompt_no_lang(
-    default_context,
-    translate_dir,
-    get_input_files,
-    make_output_path,
-    snapshot_dir,
-    update_snapshots,
-):
-    for input_file in get_input_files(translate_dir):
-        ctx = copy.copy(default_context)  # shallow clone
-        ctx.prompt_template = "Translate to English: {text}."
-        output_file = make_output_path(input_file, ".txt")
-        _TranslateBase.run(ctx, input_file, output_file)
-
-        text = output_file.read_text(encoding="utf-8")
-        assert_or_update_snapshot(
-            text,
-            f"translate/{input_file.stem}.no-lang.txt",
-            snapshot_dir,
-            update_snapshots,
-            threshold=0.9,
-        )
-
-
-def test_run_with_custom_prompt(
-    default_context,
-    translate_dir,
-    get_input_files,
-    make_output_path,
-    snapshot_dir,
-    update_snapshots,
-):
-    for input_file in get_input_files(translate_dir):
-        ctx = copy.copy(default_context)  # shallow clone
-        ctx.prompt_template = "Translate from {source_lang} to {target_lang}."
-        ctx.system_message = "You are a linguist."
-        ctx.source_lang = "it"
-        ctx.target_lang = "en"
-        output_file = make_output_path(input_file, ".txt")
-        _TranslateBase.run(ctx, input_file, output_file)
-
-        text = output_file.read_text(encoding="utf-8")
-        assert_or_update_snapshot(
-            text,
-            f"translate/{input_file.stem}.custom.txt",
             snapshot_dir,
             update_snapshots,
             threshold=0.9,
